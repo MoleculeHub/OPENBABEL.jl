8d301fa404eaee2256a7824156eb6dbfe4eb7320
Victor Cano Gil <vcanogil@gmail.com>
2025-03-30 05:35:57 -0400

    update

--- config.jl
+++ config.jl
@@ -1,5 +1,7 @@
-export libdir
-export datadir
+export LIBDIR
+export DATADIR
 
-const libdir = joinpath(openbabel_jll.find_artifact_dir(), "lib", "openbabel", "3.1.0")
-const datadir = joinpath(openbabel_jll.find_artifact_dir(), "share", "openbabel", "3.1.0")
+const LIBDIR = joinpath(openbabel_jll.find_artifact_dir(), "lib", "openbabel", "3.1.0")
+const DATADIR = joinpath(openbabel_jll.find_artifact_dir(), "share", "openbabel", "3.1.0")
+const SUPPORTED_READ_FORMATS = ["smi", "sdf", "mol", "pdb", "inchi"]
+const SUPPORTED_WRITE_FORMATS = ["smi", "sdf", "mol", "pdb", "inchi"]
--- test.jl
+++ test.jl
@@ -6,7 +6,9 @@ include("./config.jl")
 include("./utils.jl")
 include("./single_transform.jl")
 
-function test(input_smiles, io::IOBuffer=IOBuffer())
+convert_molecule("./test_smi_one.smi", "smi", "pdb") 
+
+function test(input_smiles, io::IOBuffer = IOBuffer())
     temp_file = "temp_file.smi"
     file = open(temp_file, "w")
     for smile in input_smiles
@@ -14,45 +16,51 @@ function test(input_smiles, io::IOBuffer=IOBuffer())
     end
     close(file)
     cmd = addenv(`$(obabel()) -iinchi $temp_file -osmi`,
-            "BABEL_LIBDIR" => libdir,
-            "BABEL_DATADIR" => datadir);
-    cmd = pipeline(cmd, stdout=io, stderr=devnull) |> run
+        "BABEL_LIBDIR" => libdir,
+        "BABEL_DATADIR" => datadir)
+    cmd = pipeline(cmd, stdout = io, stderr = devnull) |> run
     rm(temp_file)
     out = take!(io) |> String
     return split(out, r"\s+")
 end
 
 i = "InChI=1S/C12H19N3O/c13-9-15-7-10(8-15)6-14-12(16)11-4-2-1-3-5-11/h10-11H,1-8H2,(H,14,16)"
+i = "CCC"
 smiles = repeat([i], 10^6)
+test(smiles[1])
 @time begin
     test(smiles)
 end
 
 ##############################3
 
-
-
 run(addenv(`$(obabel()) -ismi -:"CCCCC" -oinchi`,
-          "BABEL_LIBDIR" => libdir,
-          "BABEL_DATADIR" => datadir
-          ));
+    "BABEL_LIBDIR" => LIBDIR,
+    "BABEL_DATADIR" => DATADIR
+));
 
-$run(addenv(`$(obabel()) -ismi test.smi -oxyz --gen3D`,
-          "BABEL_LIBDIR"=>joinpath(openbabel_jll.find_artifact_dir(),"lib","openbabel","3.1.0"),
-          "BABEL_DATADIR"=>joinpath(openbabel_jll.find_artifact_dir(),"share","openbabel","3.1.0"),
-          ));
+run(addenv(`$(obabel()) -ismi test_smi_one.smi -oxyz --gen3D`,
+    "BABEL_LIBDIR" => joinpath(
+        openbabel_jll.find_artifact_dir(), "lib", "openbabel", "3.1.0"),
+    "BABEL_DATADIR" => joinpath(
+        openbabel_jll.find_artifact_dir(), "share", "openbabel", "3.1.0")
+));
 
 run(addenv(`$(obabel()) filterset.sdf -osmi --filter "s='CN' s!='[N+]'"`,
-          "BABEL_LIBDIR"=>joinpath(openbabel_jll.find_artifact_dir(),"lib","openbabel","3.1.0"),
-          "BABEL_DATADIR"=>joinpath(openbabel_jll.find_artifact_dir(),"share","openbabel","3.1.0"),
-          ));
+    "BABEL_LIBDIR" => joinpath(
+        openbabel_jll.find_artifact_dir(), "lib", "openbabel", "3.1.0"),
+    "BABEL_DATADIR" => joinpath(
+        openbabel_jll.find_artifact_dir(), "share", "openbabel", "3.1.0")
+));
 
 run(addenv(`$(obabel()) test.smi -ofpt`,
-          "BABEL_LIBDIR"=>joinpath(openbabel_jll.find_artifact_dir(),"lib","openbabel","3.1.0"),
-          "BABEL_DATADIR"=>joinpath(openbabel_jll.find_artifact_dir(),"share","openbabel","3.1.0"),
-          ));
+    "BABEL_LIBDIR" => joinpath(
+        openbabel_jll.find_artifact_dir(), "lib", "openbabel", "3.1.0"),
+    "BABEL_DATADIR" => joinpath(
+        openbabel_jll.find_artifact_dir(), "share", "openbabel", "3.1.0")
+));
 
 run(addenv(`$(obabel()) test.smi -ofpt`,
-          "BABEL_LIBDIR" => libdir,
-          "BABEL_DATADIR" => datadir
-          ));
+    "BABEL_LIBDIR" => libdir,
+    "BABEL_DATADIR" => datadir
+));
