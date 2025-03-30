4f77f19e8af36f23d00b050f8238b2511a80bf2c
Victor Cano Gil <vcanogil@gmail.com>
2025-03-30 05:56:01 -0400

    update

---
+++ src/convert.jl
@@ -0,0 +1,24 @@
+export convert_molecule
+
+function make_obabel_cmd(input::String,
+        input_format::String,
+        output_format::String)
+    cmd = addenv(
+        `$(obabel()) -i$input_format $input -o$output_format`,
+        "BABEL_LIBDIR" => LIBDIR,
+        "BABEL_DATADIR" => DATADIR
+    )
+    return cmd
+end
+
+function convert_molecule(input::String,
+        input_format::String,
+        output_format::String,
+        io::IOBuffer = IOBuffer())
+    cmd = input |>
+          x -> make_obabel_cmd(x, input_format, output_format) |>
+               x -> pipeline(x, stdout = io, stderr = devnull) |>
+                    run
+    out = take!(io) |> String
+    return replace(out, r"\s+" => "")
+end
--- utils.jl
+++
@@ -1,7 +0,0 @@
-export check_if_empty
-
-function check_if_empty(input_file::String)
-    if stat(input_file).size == 0
-        throw(ArgumentError("Passed file $input_file is empty."))
-    end
-end
